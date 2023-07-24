2686b65b12c19fd27e9e8217d4cd350717e00739
TotalUmbrella <jerryruoxuzhang@gmail.com>
2023-07-24 19:49:20 +0800

    initial commit

---
+++ Assets/Script/Grab.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grab : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int layerMask = 1 << 8;
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask)) {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    }
+}
---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+
+{
+    // Start is called before the first frame update
+    public float speed = 10f;
+    private CharacterController characterController;
+    private float sensitivity = 1.8f;
+    private float xRotation = 0f;
+    public Camera camera;
+    private float jumpHeight = 900f;
+    private float gravity = -2150f;
+    private float yvelocity = 0f;
+    private float jumpBuffer = 0f;
+    private bool land = false;
+    public AudioSource landSound;
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity;
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        camera.transform.localRotation = Quaternion.Euler(xRotation, 10, 0);
+        if (Input.GetKey(KeyCode.LeftShift)){
+            speed = 15f;
+        }
+        else {
+            speed = 10f;
+        }
+        transform.Rotate(Vector3.up * mouseX);
+        Vector3 move = ((forward * Input.GetAxis("Vertical")) * speed + (right * Input.GetAxis("Horizontal")) * speed);
+
+        if (Input.GetKey(KeyCode.Space)){
+            jumpBuffer = 0.01f;
+        }
+        jumpBuffer = jumpBuffer - Time.deltaTime;
+        if (characterController.isGrounded){
+            if (jumpBuffer > 0) {
+                yvelocity = jumpHeight;
+                Debug.Log("balls");
+            }
+        }        
+        if (characterController.isGrounded){
+            if (yvelocity <= 0 ){
+                yvelocity = 0;
+            }
+        }
+
+        yvelocity += gravity * Time.deltaTime;
+
+        move = move + Vector3.up * yvelocity * Time.deltaTime * 5;
+
+        
+
+        characterController.Move(move * Time.deltaTime);
+    }
+}
